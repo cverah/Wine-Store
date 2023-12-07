dff17fe9261c57f43f87075a75d7c099e1437e7e
Cristhian Vera <cristianmario092190@gmail.com>
2023-12-06 19:45:53 -0500

    feat: fix button od IconBarMovile

--- src/utils/Header/IconBarMovile.jsx
+++ src/utils/Header/IconBarMovile.jsx
@@ -1,12 +1,16 @@
 const IconBarMovile = () => {
   return (
-    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+    <div
+      className="inset-y-0 left-0 flex items-center sm:hidden"
+      style={{ border: "1px solid white", borderRadius: "8px" }}
+    >
       {/* <!-- Mobile menu button--> */}
       <button
         type="button"
-        className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+        className="relative inline-flex items-center justify-center p-2 text-white hover:bg-red-600 focus:ring-2 focus:ring-inset focus:ring-white"
         aria-controls="mobile-menu"
         aria-expanded="false"
+        style={{ borderRadius: "8px" }}
       >
         {/* <!--
 Icon when menu is closed.
