63f1858ab66b6ab951835d4e011cfb9c509f6855
Cristhian Vera <cristianmario092190@gmail.com>
2023-12-09 15:32:05 -0500

    feat: add modal in winers

---
+++ src/utils/WindowModal.jsx
@@ -0,0 +1,42 @@
+import { Box, Button, Modal, Typography } from "@mui/material";
+import PropTypes from "prop-types";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "auto",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4, //padding
+};
+
+const WindowModal = ({ open, handleClose }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Text in a modal
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </Typography>
+        <Button onClick={handleClose}>Close Child Modal</Button>
+      </Box>
+    </Modal>
+  );
+};
+
+WindowModal.propTypes = {
+  open: PropTypes.bool,
+  handleClose: PropTypes.func,
+};
+
+export default WindowModal;
