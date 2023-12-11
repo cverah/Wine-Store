15cfdf1cb97aef6ffe73f5bb43abd8caed183998
Cristhian Vera <cristianmario092190@gmail.com>
2023-12-11 13:00:19 -0500

    feat: desing modal winer completed

--- src/components/Header/HeaderMobile.jsx
+++ src/components/Header/HeaderMobile.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 const HeaderMobile = ({ isOpen }) => {
   return (
     <div className={`sm:hidden ${isOpen ? "block" : "hidden"}`}>
@@ -6,12 +7,12 @@ const HeaderMobile = ({ isOpen }) => {
       <div className="space-y-1 px-2 pb-3 pt-2">
         {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
 
-        <a
-          href="#"
+        <Link
+          to="/"
           className=" text-white block rounded-md px-3 py-2 text-base uppercase hover:underline underline-offset-4 font-medium"
         >
-          Dashboard
-        </a>
+          Inicio
+        </Link>
         <a
           href="#"
           className="text-white block rounded-md px-3 py-2 text-base uppercase hover:underline underline-offset-4 font-medium"
