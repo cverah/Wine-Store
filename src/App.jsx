c65a9fdba72ebb5c828ecd8a58ef8f5e9ca6cb59
Cristhian Vera <cristianmario092190@gmail.com>
2023-12-16 19:06:23 -0500

    feat: add button whastapp

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Catalogue } from "./pages/Catalogue";
 import Contact from "./pages/Contact";
+import GoWhastapp from "./utils/GoWhastapp";
 
 function App() {
   //aqui link de imagen para fondo de la web
@@ -23,6 +24,7 @@ function App() {
 
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+      <GoWhastapp />
     </>
   );
 }
---
+++ src/utils/GoWhastapp.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import { FaWhatsappSquare } from "react-icons/fa";
+
+const GoWhastapp = () => {
+  return (
+    <Link to="https://api.whatsapp.com/send?phone=928646027" target="_blank">
+      <FaWhatsappSquare className="fixed z-50 rounded-xl bottom-5 right-3 text-7xl text-green-500 bg-white ransition-transform duration-200 hover:scale-110" />
+    </Link>
+  );
+};
+
+export default GoWhastapp;
